6bcd189f1f13100f3a60ea42491f714f9f696f8d
jules Ivanic <jules.ivanic@gmail.com>
2019-11-08 11:14:13 +1100

    Add missing `serialVersionUID`

--- src/main/java/io/simplesource/example/demo/repository/write/CreateAccountError.java
+++ src/main/java/io/simplesource/example/demo/repository/write/CreateAccountError.java
@@ -8,18 +8,24 @@ public abstract class CreateAccountError extends CommandError {
     }
 
     public static final class AccountAlreadyExists extends CreateAccountError {
+        private static final long serialVersionUID = 8534432619596383268L;
+
         public AccountAlreadyExists() {
             super("Account already exists");
         }
     }
 
     public static final class AccountNameNotSet extends CreateAccountError {
+        private static final long serialVersionUID = 3464543909320178223L;
+
         public AccountNameNotSet() {
             super("Account name is not set");
         }
     }
 
     public static final class OpeningBalanceLessThanZero extends CreateAccountError {
+        private static final long serialVersionUID = -7686952430796247443L;
+
         public OpeningBalanceLessThanZero() {
             super("Opening balance cannot be negative");
         }
